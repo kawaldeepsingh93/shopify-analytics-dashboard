c46680b97929b904500c4165d3c03f4408eb6ae8
kawaldeepsingh93 <kawaldeepsingh93@gmail.com>
2025-10-27 00:18:20 +0530

    Base URL issue updated

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
+  basePath: process.env.NODE_ENV === 'production' ? '/shopify-analytics-dashboard' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/shopify-analytics-dashboard/' : '',
   images: {
     unoptimized: true
   }
